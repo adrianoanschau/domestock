f4c45ef181621b5cdd3eacdfdaae654d1f3d549b
Adriano Anschau <adrianoanschau@gmail.com>
2024-10-10 11:26:40 -0300

    change index.php

--- public/index.php
+++ public/index.php
@@ -1,3 +1,20 @@
+<?php
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
+// Determine if the application is in maintenance mode...
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+    require $maintenance;
+}
+
+// Register the Composer autoloader...
+require __DIR__.'/../vendor/autoload.php';
+
+// Bootstrap Laravel and handle the request...
+//(require_once __DIR__.'/../bootstrap/app.php')
+//    ->handleRequest(Request::capture());
+?>
 <!doctype html>
 <html lang="en">
 <head>
@@ -10,24 +27,7 @@
 <body>
 Aqui estou
 <pre>
-    <?php var_dump($_SERVER); ?>
+    <?php var_dump($_ENV); ?>
 </pre>
 </body>
 </html>
-
-<?php
-//use Illuminate\Http\Request;
-//
-//define('LARAVEL_START', microtime(true));
-//
-//// Determine if the application is in maintenance mode...
-//if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-//    require $maintenance;
-//}
-//
-//// Register the Composer autoloader...
-//require __DIR__.'/../vendor/autoload.php';
-//
-//// Bootstrap Laravel and handle the request...
-//(require_once __DIR__.'/../bootstrap/app.php')
-//    ->handleRequest(Request::capture());
