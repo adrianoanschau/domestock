1c499c0e24402a936b9b73fcc1a3029a094567d9
Adriano Anschau <adrianoanschau@gmail.com>
2024-10-10 11:31:52 -0300

    change cloudbuild.yaml

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,5 +2,5 @@
 
 return [
     App\Providers\AppServiceProvider::class,
-    App\Providers\AdminPanelProvider::class,
+//    App\Providers\AdminPanelProvider::class,
 ];
--- public/index.php
+++ public/index.php
@@ -12,22 +12,5 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 require __DIR__.'/../vendor/autoload.php';
 
 // Bootstrap Laravel and handle the request...
-//(require_once __DIR__.'/../bootstrap/app.php')
-//    ->handleRequest(Request::capture());
-?>
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Teste</title>
-</head>
-<body>
-Aqui estou
-<pre>
-    <?php var_dump($_ENV); ?>
-</pre>
-</body>
-</html>
+(require_once __DIR__.'/../bootstrap/app.php')
+    ->handleRequest(Request::capture());
